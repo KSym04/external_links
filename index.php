86781f12ed1ef2502adfc5f3789f0a51565dc6d9
KSym04 <ken.villanueva21@gmail.com>
2014-07-03 15:12:20 +0800

    First version release.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: External Links for Osclass
 Plugin URI: http://www.dopethemes.com/plugins/external-links-for-osclass/
-Description: This plugin automatically convert text URLs to hyoerlinks and adds rel=&quot;nofollow&quot; and target=&quot;_blank&quot;, for all the external links of your website item post.
+Description: This plugin convert text URLs to hyperlinks and adds rel=&quot;nofollow&quot; and target=&quot;_blank&quot;, for all the external links of your website item post.
 Version: 1.0.0
 Author: DopeThemes
 Author URI: http://www.dopethemes.com/
@@ -12,5 +12,50 @@ Support URI: http://www.dopethemes.com/contact-us/
 */
 
 // == FUNCTION == //
+function external_links_make_url_clickable_cb( $matches ) {
+  $ret = '';
+  $url = $matches[2];
 
-// == HOOKS AND FILTERS == //
+  if ( empty( $url ) )
+    return $matches[0];
+  if ( in_array( substr( $url, -1 ), array( '.', ',', ';', ':' ) ) === true ) {
+    $ret = substr( $url, -1 );
+    $url = substr( $url, 0, strlen( $url )-1 );
+  }
+
+  // Check if the url is external
+  $url_data = parse_url( $url );
+  if( $url_data['host'] != $_SERVER['HTTP_HOST'] ) {
+    $url_follow = 'rel="nofollow" target="_blank"';
+  }
+  return $matches[1] . "<a href=\"$url\" ".$url_follow .">$url</a>" . $ret;
+}
+
+function external_links_make_web_ftp_clickable_cb( $matches ) {
+  $ret = '';
+  $dest = $matches[2];
+  $dest = 'http://' . $dest;
+
+  if ( empty( $dest ) )
+    return $matches[0];
+  if ( in_array( substr( $dest, -1 ), array( '.', ',', ';', ':' ) ) === true ) {
+    $ret = substr( $dest, -1 );
+    $dest = substr( $dest, 0, strlen( $dest )-1 );
+  }
+  return $matches[1] . "<a href=\"$dest\" target=\"_blank\" rel=\"nofollow\">$dest</a>" . $ret;
+}
+
+function external_links_make_email_clickable_cb( $matches ) {
+  $email = $matches[2] . '@' . $matches[3];
+  return $matches[1] . "<a href=\"mailto:$email\">$email</a>";
+}
+
+function external_links_make_clickable( $ret ) {
+  $ret = ' ' . $ret;
+  $ret = preg_replace_callback( '#([\s>])([\w]+?://[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]*)#is', 'external_links_make_url_clickable_cb', $ret );
+  $ret = preg_replace_callback( '#([\s>])((www|ftp)\.[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]*)#is', 'external_links_make_web_ftp_clickable_cb', $ret );
+  $ret = preg_replace_callback( '#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', 'external_links_make_email_clickable_cb', $ret );
+  $ret = preg_replace( "#(<a( [^>]+?>|>))<a [^>]+?>([^>]+?)</a></a>#i", "$1$3</a>", $ret );
+  $ret = trim( $ret );
+  return $ret;
+}
--- readme.txt
+++ readme.txt
@@ -5,20 +5,23 @@ Requires at least: 3.3.2
 Tested up to: 3.3.2
 Stable tag: 3.3.2
 
-This plugin automatically convert text URLs to hyoerlinks and adds rel=&quot;nofollow&quot; and target=&quot;_blank&quot;, for all the external links of your website item post.
+This plugin convert text URLs to hyperlinks and adds rel=&quot;nofollow&quot; and target=&quot;_blank&quot;, for all the external links of your website item post.
 
 
 == Description ==
-This plugin automatically convert text URLs to hyoerlinks and adds rel=&quot;nofollow&quot; and target=&quot;_blank&quot;, for all the external links of your website item post.
+This plugin convert text URLs to hyperlinks and adds rel=&quot;nofollow&quot; and target=&quot;_blank&quot;, for all the external links of your website item post.
 
 
 == Installation ==
-1. Upload `insert_on_header_footer/` to the `/oc-content/plugins/` directory
+1. Upload `external_links/` to the `/oc-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in Osclass
-3. Sit back and enjoy
+3. On how to use please check FAQ's
 
 
 == Frequently Asked Questions ==
+1. Q: How to use this plugin?
+A: Just wrap the string to external_links_make_clickable() for example: external_links_make_clickable( osc_item_description )
+
 1. Q: Where's the admin settings?
 A: Currently, There is no available configuration panel for this plugin.
 
