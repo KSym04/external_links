dec9b02e79c95c2566afa53e60bb54ad06eb6bfb
KSym04 <ken.villanueva21@gmail.com>
2015-05-10 19:23:45 +0800

    Minor code cleanup.

--- index.php
+++ index.php
@@ -11,13 +11,21 @@ Short Name: external_links
 Support URI: http://www.dopethemes.com/contact-us/
 */
 
-// == FUNCTION == //
+/**
+ * Convert url string to make url clickable
+ *
+ * @since 1.0.0
+ * @access public
+ * @return string
+ */
 function external_links_make_url_clickable_cb( $matches ) {
-  $ret = '';
+
+  $ret = NULL;
   $url = $matches[2];
 
   if ( empty( $url ) )
     return $matches[0];
+
   if ( in_array( substr( $url, -1 ), array( '.', ',', ';', ':' ) ) === true ) {
     $ret = substr( $url, -1 );
     $url = substr( $url, 0, strlen( $url )-1 );
@@ -28,34 +36,60 @@ function external_links_make_url_clickable_cb( $matches ) {
   if( $url_data['host'] != $_SERVER['HTTP_HOST'] ) {
     $url_follow = 'rel="nofollow" target="_blank"';
   }
+
   return $matches[1] . "<a href=\"$url\" ".$url_follow .">$url</a>" . $ret;
 }
 
+/**
+ * Convert ftp url string clickable
+ *
+ * @since 1.0.0
+ * @access public
+ * @return string
+ */
 function external_links_make_web_ftp_clickable_cb( $matches ) {
-  $ret = '';
+
+  $ret = NULL;
   $dest = $matches[2];
   $dest = 'http://' . $dest;
 
   if ( empty( $dest ) )
     return $matches[0];
+
   if ( in_array( substr( $dest, -1 ), array( '.', ',', ';', ':' ) ) === true ) {
     $ret = substr( $dest, -1 );
     $dest = substr( $dest, 0, strlen( $dest )-1 );
   }
+
   return $matches[1] . "<a href=\"$dest\" target=\"_blank\" rel=\"nofollow\">$dest</a>" . $ret;
 }
 
+/**
+ * Convert email string to mailto:
+ *
+ * @since 1.0.0
+ * @access public
+ * @return string
+ */
 function external_links_make_email_clickable_cb( $matches ) {
   $email = $matches[2] . '@' . $matches[3];
   return $matches[1] . "<a href=\"mailto:$email\">$email</a>";
 }
 
+/**
+ * Process links or any valid url clickable
+ *
+ * @since 1.0.0
+ * @access public
+ * @return string
+ */
 function external_links_make_clickable( $ret ) {
-  $ret = ' ' . $ret;
+
   $ret = preg_replace_callback( '#([\s>])([\w]+?://[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]*)#is', 'external_links_make_url_clickable_cb', $ret );
   $ret = preg_replace_callback( '#([\s>])((www|ftp)\.[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]*)#is', 'external_links_make_web_ftp_clickable_cb', $ret );
   $ret = preg_replace_callback( '#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', 'external_links_make_email_clickable_cb', $ret );
   $ret = preg_replace( "#(<a( [^>]+?>|>))<a [^>]+?>([^>]+?)</a></a>#i", "$1$3</a>", $ret );
   $ret = trim( $ret );
+
   return $ret;
 }
