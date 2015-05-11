ae25a7ebb0671a14b95222ec50e96faced5f7c97
KSym04 <ken.villanueva21@gmail.com>
2015-05-11 11:19:22 +0800

    Version 1.0.1

--- index.php
+++ index.php
@@ -16,14 +16,18 @@ Support URI: http://www.dopethemes.com/contact-us/
  * Set some defaults on after installation
  */
 function external_links_call_after_install() {
-  osc_set_preference( 'new_tab', '', 'plugin-external_links', 'STRING' );
+  osc_set_preference( 'new_window', '1', 'plugin-external_links', 'BOOLEAN' );
+  osc_set_preference( 'add_no_follow', '1', 'plugin-external_links', 'BOOLEAN' );
+  osc_set_preference( 'auto_convert_emails', '0', 'plugin-external_links', 'BOOLEAN' );
 }
 
 /**
  * Delete preferences value after uninstall
  */
 function external_links_call_after_uninstall() {
-  osc_delete_preference( 'new_tab', 'plugin-external_links' );
+  osc_delete_preference( 'new_window', 'plugin-external_links' );
+  osc_delete_preference( 'add_no_follow', 'plugin-external_links' );
+  osc_delete_preference( 'auto_convert_emails', 'plugin-external_links' );
 }
 
 /**
@@ -37,7 +41,9 @@ function external_links_actions() {
   // Save settings
   if ( Params::getParam( 'option' ) == 'settings_saved' ) {
     // set data
-    osc_set_preference( 'new_tab', Params::getParam( "new_tab", false, false ), 'plugin-external_links', 'STRING' );
+    osc_set_preference( 'new_window', Params::getParam( "new_window", false, false ), 'plugin-external_links', 'BOOLEAN' );
+    osc_set_preference( 'add_no_follow', Params::getParam( "add_no_follow", false, false ), 'plugin-external_links', 'BOOLEAN' );
+    osc_set_preference( 'auto_convert_emails', Params::getParam( "auto_convert_emails", false, false ), 'plugin-external_links', 'BOOLEAN' );
 
     // return message
     osc_add_flash_ok_message( __( 'Settings saved.', 'external_links' ), 'admin' );
@@ -81,8 +87,9 @@ function external_links_make_url_clickable_cb( $matches ) {
   $ret = NULL;
   $url = $matches[2];
 
-  if ( empty( $url ) )
+  if ( empty( $url ) ) {
     return $matches[0];
+  }
 
   if ( in_array( substr( $url, -1 ), array( '.', ',', ';', ':' ) ) === true ) {
     $ret = substr( $url, -1 );
@@ -92,10 +99,18 @@ function external_links_make_url_clickable_cb( $matches ) {
   // Check if the url is external
   $url_data = parse_url( $url );
   if( $url_data['host'] != $_SERVER['HTTP_HOST'] ) {
-    $url_follow = 'rel="nofollow" target="_blank"';
+    if( osc_get_preference( 'new_window', 'plugin-external_links' ) == true ) {
+      $target_value = 'target="_blank"';
+    }
+
+    if( osc_get_preference( 'add_no_follow', 'plugin-external_links' ) == true ) {
+      $follow_value = 'rel="nofollow"';
+    }
+
+    $url_follow = sprintf( '%s %s', $follow_value, $target_value );
   }
 
-  return $matches[1] . "<a href=\"$url\" ".$url_follow .">$url</a>" . $ret;
+  return sprintf( '%1s <a href="%2s" %3s>%4s</a> %5s', $matches[1], $url, $url_follow, $url, $ret );
 }
 
 /**
@@ -111,15 +126,20 @@ function external_links_make_web_ftp_clickable_cb( $matches ) {
   $dest = $matches[2];
   $dest = 'http://' . $dest;
 
-  if ( empty( $dest ) )
+  if ( empty( $dest ) ) {
     return $matches[0];
+  }
 
   if ( in_array( substr( $dest, -1 ), array( '.', ',', ';', ':' ) ) === true ) {
     $ret = substr( $dest, -1 );
     $dest = substr( $dest, 0, strlen( $dest )-1 );
   }
 
-  return $matches[1] . "<a href=\"$dest\" target=\"_blank\" rel=\"nofollow\">$dest</a>" . $ret;
+  if( osc_get_preference( 'new_window', 'plugin-external_links' ) == true ) {
+    $target_value = 'target="_blank"';
+  }
+
+  return sprintf( '%1s<a href="%2s" %3s>%4s</a>%5s', $matches[1], $dest, $target_value, $url, $ret );
 }
 
 /**
@@ -131,7 +151,7 @@ function external_links_make_web_ftp_clickable_cb( $matches ) {
  */
 function external_links_make_email_clickable_cb( $matches ) {
   $email = $matches[2] . '@' . $matches[3];
-  return $matches[1] . "<a href=\"mailto:$email\">$email</a>";
+  return sprintf( '%1s<a href="mailto:%2s">%3s</a>', $matches[1], $email, $email );
 }
 
 /**
@@ -143,9 +163,17 @@ function external_links_make_email_clickable_cb( $matches ) {
  */
 function external_links_make_clickable( $ret ) {
 
+  // convert txt urls
   $ret = preg_replace_callback( '#([\s>])([\w]+?://[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]*)#is', 'external_links_make_url_clickable_cb', $ret );
+
+  // convert ftp
   $ret = preg_replace_callback( '#([\s>])((www|ftp)\.[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]*)#is', 'external_links_make_web_ftp_clickable_cb', $ret );
-  $ret = preg_replace_callback( '#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', 'external_links_make_email_clickable_cb', $ret );
+
+  // convert emails
+  if( osc_get_preference( 'auto_convert_emails', 'plugin-external_links' ) == 1 ) {
+    $ret = preg_replace_callback( '#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', 'external_links_make_email_clickable_cb', $ret );
+  }
+
   $ret = preg_replace( "#(<a( [^>]+?>|>))<a [^>]+?>([^>]+?)</a></a>#i", "$1$3</a>", $ret );
   $ret = trim( $ret );
 
