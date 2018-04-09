7ca92571235f67df14100338008d4bedc67cc239
LAPTOP-AR62V6PB\pc <dev@dopethemes.com>
2018-04-09 16:44:03 +0800

    Version 1.0.4

--- index.php
+++ index.php
@@ -115,99 +115,128 @@ function external_links_admin_style() {
 osc_add_hook( 'init_admin', 'external_links_admin_style' );
 
 /**
- * external_links_make_url_clickable_cb
+ * _external_links_url_checker
  *
- * Convert url string to make url clickable
+ * Check if url is not same as host
  *
  * @since 1.0.0
- * @access public
+ * @access private
+ *
+ * @param array
  * @return string
  */
-function external_links_make_url_clickable_cb( $matches ) {
-	$ret = NULL;
-	$url = $matches[2];
+function _external_links_url_checker( $url ) {
+	$url_data = parse_url( $url );
+	if( $url_data['host'] !== $_SERVER['HTTP_HOST'] ) {
+	  $data_attr = array();
 
-  	if ( empty( $url ) ) {
-    	return $matches[0];
-  	}
+	  // target attributes
+	  if( osc_get_preference( 'new_window', 'plugin-external_links' ) == true ) {
+		  $data_attr[] = 'target="_blank"';
+	  }
 
-  	if ( in_array( substr( $url, -1 ), array( '.', ',', ';', ':' ) ) === true ) {
-    	$ret = substr( $url, -1 );
-		$url = substr( $url, 0, strlen( $url )-1 );
-	}
+	  // rel attributes
+	  if( osc_get_preference( 'add_no_follow', 'plugin-external_links' ) == true ) {
+			$rel_value[] = 'nofollow';
+	  }
 
-  	// Check if the url is external
-  	$url_data = parse_url( $url );
-  	if( $url_data['host'] !== $_SERVER['HTTP_HOST'] ) {
-		$data_attr = array();
+	  if( osc_get_preference( 'add_no_opener', 'plugin-external_links' ) == true ) {
+		  $rel_value[] = 'noopener';
+	  }
 
-		// target attributes
-    	if( osc_get_preference( 'new_window', 'plugin-external_links' ) === true ) {
-    		$data_attr[] = 'target="_blank"';
-		}
+	  if( $rel_value ) {
+		  $data_attr[] = sprintf( 'rel="%s"', implode( ' ', $rel_value ) );
+	  }
 
-		// rel attributes
-    	if( osc_get_preference( 'add_no_follow', 'plugin-external_links' ) === true ) {
-      		$rel_value[] = 'nofollow';
-		}
-
-    	if( osc_get_preference( 'add_no_opener', 'plugin-external_links' ) === true ) {
-			$rel_value[] = 'noopener';
-		}
-
-		if( $rel_value ) {
-			$data_attr[] = sprintf( 'rel="%s"', implode( ' ', $rel_value ) );
-		}
-
-		// format attributes
-		$finalize_attr = implode( ' ', $data_attr );
-	}
-
-	return sprintf( '%1$s <a href="%2$s" %3$s>%4$s</a> %5$s', $matches[1], $url, $finalize_attr, $url, $ret );
+	  // format attributes
+	  return $finalize_attr = implode( ' ', $data_attr );
+  }
 }
 
 /**
- * external_links_make_web_ftp_clickable_cb
+ * _external_link_osclass_make_url_clickable_cb
  *
- * Convert ftp url string clickable
+ * Callback to convert URI match to HTML A element.
  *
  * @since 1.0.0
- * @access public
+ * @access private
+ *
+ * @param array
  * @return string
  */
-function external_links_make_web_ftp_clickable_cb( $matches ) {
-	$ret = NULL;
+function _external_link_osclass_make_url_clickable_cb( $matches ) {
+	$url = $matches[2];
+
+	if ( ')' == $matches[3] && strpos( $url, '(' ) ) {
+		// If the trailing character is a closing parethesis, and the URL has an opening parenthesis in it, add the closing parenthesis to the URL.
+		// Then we can let the parenthesis balancer do its thing below.
+		$url .= $matches[3];
+		$suffix = '';
+	} else {
+		$suffix = $matches[3];
+	}
+
+	// Include parentheses in the URL only if paired
+	while ( substr_count( $url, '(' ) < substr_count( $url, ')' ) ) {
+		$suffix = strrchr( $url, ')' ) . $suffix;
+		$url = substr( $url, 0, strrpos( $url, ')' ) );
+	}
+
+	$url = osc_sanitize_url( $url );
+	if ( empty( $url ) ) {
+		return $matches[0];
+	}
+
+	$finalize_attr = _external_links_url_checker( $url );
+	return $matches[1] . "<a href=\"$url\" $finalize_attr>$url</a>" . $suffix;
+}
+
+/**
+ * _external_links_osclass_make_web_ftp_clickable_cb
+ *
+ * Callback to convert URL match to HTML A element.
+ *
+ * @since 1.0.0
+ * @access private
+ *
+ * @param array $matches Single Regex Match.
+ * @return string HTML A element with URL address.
+ */
+function _external_links_osclass_make_web_ftp_clickable_cb( $matches ) {
+	$ret = '';
 	$dest = $matches[2];
 	$dest = 'http://' . $dest;
 
+	// removed trailing [.,;:)] from URL
+	if ( in_array( substr($dest, -1), array('.', ',', ';', ':', ')') ) === true ) {
+		$ret = substr($dest, -1);
+		$dest = substr($dest, 0, strlen($dest)-1);
+	}
+
+	$dest = osc_sanitize_url( $dest );
 	if ( empty( $dest ) ) {
 		return $matches[0];
 	}
 
-	if ( in_array( substr( $dest, -1 ), array( '.', ',', ';', ':' ) ) === true ) {
-		$ret = substr( $dest, -1 );
-		$dest = substr( $dest, 0, strlen( $dest )-1 );
-	}
-
-	if( osc_get_preference( 'new_window', 'plugin-external_links' ) === true ) {
-		$target_value = 'target="_blank"';
-	}
-
-	return sprintf( '%1s<a href="%2s" %3s>%4s</a>%5s', $matches[1], $dest, $target_value, $matches[2], $ret );
+  	// Check if the url is external
+  	$finalize_attr = _external_links_url_checker( $dest );
+	return $matches[1] . "<a href=\"$dest\" $finalize_attr>$dest</a>$ret";
 }
 
 /**
- * external_links_make_email_clickable_cb
+ * _external_links_osclass_make_email_clickable_cb
  *
  * Convert email string to mailto:
  *
- * @since 1.0.0
- * @access public
+ * @since  1.0.0
+ * @access private
+ *
+ * @param  array
  * @return string
  */
-function external_links_make_email_clickable_cb( $matches ) {
+function _external_links_osclass_make_email_clickable_cb( $matches ) {
 	$email = $matches[2] . '@' . $matches[3];
-	return sprintf( '%1s<a href="mailto:%2s">%3s</a>', $matches[1], $email, $email );
+	return $matches[1] . "<a href=\"mailto:$email\">$email</a>";
 }
 
 /**
@@ -219,20 +248,63 @@ function external_links_make_email_clickable_cb( $matches ) {
  * @access public
  * @return string
  */
-function external_links_make_clickable( $ret ) {
-	// convert txt urls
-	$ret = preg_replace_callback( '#([\s>])([\w]+?://[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]*)#is', 'external_links_make_url_clickable_cb', $ret );
+function external_links_make_clickable( $text ) {
+	$r = '';
+	$textarr = preg_split( '/(<[^<>]+>)/', $text, -1, PREG_SPLIT_DELIM_CAPTURE ); // split out HTML tags
+	$nested_code_pre = 0; // Keep track of how many levels link is nested inside <pre> or <code>
+	foreach ( $textarr as $piece ) {
 
-	// convert ftp
-	$ret = preg_replace_callback( '#([\s>])((www|ftp)\.[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]*)#is', 'external_links_make_web_ftp_clickable_cb', $ret );
+		if ( preg_match( '|^<code[\s>]|i', $piece ) || preg_match( '|^<pre[\s>]|i', $piece ) || preg_match( '|^<script[\s>]|i', $piece ) || preg_match( '|^<style[\s>]|i', $piece ) )
+			$nested_code_pre++;
+		elseif ( $nested_code_pre && ( '</code>' === strtolower( $piece ) || '</pre>' === strtolower( $piece ) || '</script>' === strtolower( $piece ) || '</style>' === strtolower( $piece ) ) )
+			$nested_code_pre--;
 
-	// convert emails
-	if( osc_get_preference( 'auto_convert_emails', 'plugin-external_links' ) === true ) {
-		$ret = preg_replace_callback( '#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', 'external_links_make_email_clickable_cb', $ret );
+		if ( $nested_code_pre || empty( $piece ) || ( $piece[0] === '<' && ! preg_match( '|^<\s*[\w]{1,20}+://|', $piece ) ) ) {
+			$r .= $piece;
+			continue;
+		}
+
+		// Long strings might contain expensive edge cases ...
+		if ( 10000 < strlen( $piece ) ) {
+			// ... break it up
+			foreach ( _split_str_by_whitespace( $piece, 2100 ) as $chunk ) { // 2100: Extra room for scheme and leading and trailing paretheses
+				if ( 2101 < strlen( $chunk ) ) {
+					$r .= $chunk; // Too big, no whitespace: bail.
+				} else {
+					$r .= make_clickable( $chunk );
+				}
+			}
+		} else {
+			$ret = " $piece "; // Pad with whitespace to simplify the regexes
+
+			$url_clickable = '~
+				([\\s(<.,;:!?])                                        # 1: Leading whitespace, or punctuation
+				(                                                      # 2: URL
+					[\\w]{1,20}+://                                # Scheme and hier-part prefix
+					(?=\S{1,2000}\s)                               # Limit to URLs less than about 2000 characters long
+					[\\w\\x80-\\xff#%\\~/@\\[\\]*(+=&$-]*+         # Non-punctuation URL character
+					(?:                                            # Unroll the Loop: Only allow puctuation URL character if followed by a non-punctuation URL character
+						[\'.,;:!?)]                            # Punctuation URL character
+						[\\w\\x80-\\xff#%\\~/@\\[\\]*(+=&$-]++ # Non-punctuation URL character
+					)*
+				)
+				(\)?)                                                  # 3: Trailing closing parenthesis (for parethesis balancing post processing)
+			~xS'; // The regex is a non-anchored pattern and does not have a single fixed starting character.
+			      // Tell PCRE to spend more time optimizing since, when used on a page load, it will probably be used several times.
+
+			$ret = preg_replace_callback( $url_clickable, '_external_link_osclass_make_url_clickable_cb', $ret );
+
+			$ret = preg_replace_callback( '#([\s>])((www|ftp)\.[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]+)#is', '_external_links_osclass_make_web_ftp_clickable_cb', $ret );
+
+			if( osc_get_preference( 'auto_convert_emails', 'plugin-external_links' ) == true ) {
+				$ret = preg_replace_callback( '#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', '_external_links_osclass_make_email_clickable_cb', $ret );
+			}
+
+			$ret = substr( $ret, 1, -1 ); // Remove our whitespace padding.
+			$r .= $ret;
+		}
 	}
 
-  	$ret = preg_replace( "#(<a( [^>]+?>|>))<a [^>]+?>([^>]+?)</a></a>#i", "$1$3</a>", $ret );
-  	$ret = trim( $ret );
-
-	return $ret;
+	// Cleanup of accidental links within links
+	return preg_replace( '#(<a([ \r\n\t]+[^>]+?>|>))<a [^>]+?>([^>]+?)</a></a>#i', "$1$3</a>", $r );
 }
